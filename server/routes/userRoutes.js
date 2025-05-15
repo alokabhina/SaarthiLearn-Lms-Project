4b119c0a80a45180e79f1604689120b1d2b7b92e
alokabhinandan <alokabhinandan123@gmail.com>
2025-05-15 20:11:27 +0530

    Removed invoice code and updated links

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,15 +1,16 @@
-import express from 'express'
+import express from 'express';
+import { ClerkExpressRequireAuth } from '@clerk/express';
 import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from '../controllers/userController.js';
 
+const userRouter = express.Router();
+const authMiddleware = ClerkExpressRequireAuth(); // Clerk's middleware
 
-const userRouter = express.Router()
-
-// Get user Data
-userRouter.get('/data', getUserData)
-userRouter.post('/purchase', purchaseCourse)
-userRouter.get('/enrolled-courses', userEnrolledCourses)
-userRouter.post('/update-course-progress', updateUserCourseProgress)
-userRouter.post('/get-course-progress', getUserCourseProgress)
-userRouter.post('/add-rating', addUserRating)
+// Routes
+userRouter.get('/data', authMiddleware, getUserData);
+userRouter.post('/purchase-course', authMiddleware, purchaseCourse);
+userRouter.get('/enrolled-courses', authMiddleware, userEnrolledCourses);
+userRouter.post('/update-course-progress', authMiddleware, updateUserCourseProgress);
+userRouter.post('/get-course-progress', authMiddleware, getUserCourseProgress);
+userRouter.post('/add-rating', authMiddleware, addUserRating);
 
 export default userRouter;
